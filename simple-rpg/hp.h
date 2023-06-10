651c83536c126207ad16d0e71994240733bc10e4
derungo <patrick.aiken@gmail.com>
2023-06-09 23:47:09 -0400

    getter and setter for hp pools

---
+++ simple-rpg/hp.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "hitpointtypes.h"
+
+class hp {
+    public:
+
+    // returns true if success set
+    bool setMaxHP(hptype new_max_hp)
+    {
+        if (new_max_hp < 1)
+            return false;
+
+        MaxHP = new_max_hp;
+
+        if (CurrentHP > MaxHP)
+            CurrentHP = MaxHP;
+
+        return true;
+
+    };
+
+    hptype getMaxHP() {
+        return MaxHp;
+    };
+    hptype getCurrentHP() {
+        return CurrentHP;
+
+    };
+    
+    // take damage
+    void TakeDamage(hptype damage) {
+        if (damage > CurrentHP) {
+            CurrentHP = 0;
+
+        return;
+        }
+    
+
+        CurrentHP -= damage;
+    }
+
+
+  // healing  
+void heal(hptype amount) {
+    if (amount + CurrentHP > MaxHP) {
+       CurrentHP = MaxHP;
+       return; 
+    }
+
+    CurrentHP += amount;
+
+}
+
+
+
+    private:
+        hptype ShieldHP;
+        hptype CurrentHP;
+        hptype MaxHP;
+
+};
